3df94dbaf6338c571cdee3affdf66663828a3846
Luis Antonio Parrado <luisprmat@gmail.com>
2021-11-09 00:34:45 -0500

    Edit Form

---
+++ resources/js/components/companies/CompaniesEdit.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 mb-4 rounded relative" role="alert" v-if="errors && errors.length">
+        <strong class="font-bold">¡Lo sentimos!</strong>
+        <span class="block sm:inline"> No pudimos procesar tu solicitud por los siguientes motivos:</span>
+        <ul class="list-disc px-4 mt-2">
+            <li v-for="item in errors" :key="item">{{ item }}</li>
+        </ul>
+        <!-- <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+            <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+        </span> TODO: Activate close button-->
+    </div>
+
+    <form class="space-y-6" v-on:submit.prevent="saveCompany">
+        <div class="space-y-4 rounded-md shadow-sm">
+            <div>
+                <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+                <div class="mt-1">
+                    <input type="text" name="name" id="name"
+                           class="block mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                           v-model="company.name">
+                </div>
+            </div>
+
+            <div>
+                <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                <div class="mt-1">
+                    <input type="text" name="email" id="email"
+                           class="block mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                           v-model="company.email">
+                </div>
+            </div>
+
+            <div>
+                <label for="address" class="block text-sm font-medium text-gray-700">Address</label>
+                <div class="mt-1">
+                    <input type="text" name="address" id="address"
+                           class="block mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                           v-model="company.address">
+                </div>
+            </div>
+
+            <div>
+                <label for="website" class="block text-sm font-medium text-gray-700">Website</label>
+                <div class="mt-1">
+                    <input type="text" name="website" id="website"
+                           class="block mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                           v-model="company.website">
+                </div>
+            </div>
+        </div>
+
+        <button type="submit"
+                class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase bg-gray-800 rounded-md border border-transparent ring-gray-300 transition duration-150 ease-in-out hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring disabled:opacity-25">
+            Save
+        </button>
+    </form>
+</template>
+
+<script>
+import { onMounted } from 'vue';
+import useCompanies from '../../composables/companies';
+
+export default {
+    props: {
+        id: {
+            required: true,
+            type: String
+        }
+    },
+
+    setup(props) {
+        const { errors, company, getCompany, updateCompany } = useCompanies()
+
+        onMounted(getCompany(props.id))
+
+        const saveCompany = async () => {
+            await updateCompany(props.id)
+        }
+
+        return {
+            errors,
+            company,
+            saveCompany
+        }
+    }
+}
+</script>
--- resources/js/composables/companies.js
+++ resources/js/composables/companies.js
@@ -4,12 +4,18 @@ import { useRouter } from 'vue-router';
 
 export default function useCompanies() {
     const companies = ref([])
+    const company = ref([])
     const router = useRouter()
     const errors = ref('')
 
     const getCompanies = async () => {
         let response = await axios.get('/api/companies')
         companies.value = response.data.data;
+    }
+
+    const getCompany = async (id) => {
+        let response = await axios.get(`/api/companies/${id}`)
+        company.value = response.data.data;
     }
 
     const storeCompany = async (data) => {
@@ -26,15 +32,32 @@ export default function useCompanies() {
         }
     }
 
+    const updateCompany = async (id) => {
+        errors.value = []
+        try {
+            await axios.put(`/api/companies/${id}`, company.value)
+            await router.push({ name: 'companies.index' })
+        } catch (e) {
+            if (e.response.status === 422) {
+                for (const key in e.response.data.errors) {
+                    errors.value.push(e.response.data.errors[key][0])
+                }
+            }
+        }
+    }
+
     const destroyCompany = async (id) => {
         await axios.delete(`/api/companies/${id}`)
     }
 
     return {
         companies,
+        company,
         errors,
         getCompanies,
+        getCompany,
         storeCompany,
+        updateCompany,
         destroyCompany
     }
 }
